2649c7f0aa43f09e461797495810e6f539c546f2
Nataliia Hirniak <gn.natalia19@gmail.com>
2023-05-04 17:53:41 +0100

    Fix bugs: divide logic into variables, fix mistake with checking ingredients

--- coffee_machine/main.py
+++ coffee_machine/main.py
@@ -3,44 +3,47 @@ money = 0
 is_on = True
 
 
-def make_coffee(order, resources):
+def make_coffee(order):
     drink_data = MENU[order]["ingredients"]
-    for resource in resources:
-        resources[resource] -= drink_data[resource]
+    for item in drink_data:
+        resources[item] -= drink_data[item]
 
 
-def process_coins(order, resources):
+def check_transaction(total, order):
     global money
-    print("Please insert coins.")
-    quarters = int(input("How many quarters?: ")) * 0.25
-    dimes = int(input("How many dimes?: ")) * 0.10
-    nickles = int(input("How many nickles?: ")) * 0.5
-    pennies = int(input("How many pennies?: ")) * 0.1
-
-    sum_coins = sum([quarters, dimes, nickles, pennies])
     drink_cost = MENU[order]["cost"]
-
-    if drink_cost > sum_coins:
+     
+    if drink_cost > total:
         print("Sorry there is not enough coins")
         return False
     else:
-        change = round(sum_coins - drink_cost, 2)
+        change = round(total - drink_cost, 2)
         money += drink_cost
         print(f"Here is your change S{change}")
         print(f"Here is your {order}. Enjoy!")
-        return make_coffee(order, resources)
+        return make_coffee(order)
 
 
-def check_ingredients(order, resources):
+def process_coins():
+    print("Please insert coins.")
+    total = int(input("How many quarters?: ")) * 0.25
+    total += int(input("How many dimes?: ")) * 0.10
+    total += int(input("How many nickles?: ")) * 0.5
+    total += int(input("How many pennies?: ")) * 0.1
+    
+    return total
+   
+
+def check_ingredients(order):
     drink_data = MENU[order]["ingredients"]
     for resource in resources:
         if resources[resource] < drink_data[resource]:
             print(f"Sorry there is not enough {resource}.")
             return False
         else:
-            return process_coins(order, resources)
-                
-
+            return True
+            
+          
 while is_on:
     order = input('''What would you like? 
     (espresso - $1.5/latte - $2.5 /cappuccino - $3): ''').lower()
@@ -49,6 +52,7 @@ while is_on:
     for item in MENU:
         if order == item:
             drink = item
+
     if order == "off":
         is_on = False
     elif order == "report":
@@ -58,11 +62,12 @@ while is_on:
                 ingredient += "g"
             else: 
                 ingredient += "ml"
-        print(f"{resource}: {ingredient}")
+            print(f"{resource}: {ingredient}")
         print(f"Money: ${money}")
     
     elif order == drink and drink != "":
-        check_ingredients(order, resources)
+        if check_ingredients(order):
+            check_transaction(process_coins(), order)
     else:
         print("Please enter valid drink")
         is_on = False
